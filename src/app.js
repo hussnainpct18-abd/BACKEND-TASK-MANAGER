a67439c4cfcdb96156691285a4dd1cf0c396ad98
Hussnain <hussnainpct18@gmail.com>
2026-05-31 14:40:31 +0500

    CORS and vercel.json fix

--- src/app.js
+++ src/app.js
@@ -10,17 +10,27 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
+const corsOptions = {
     origin: "https://task-manager-frontend-nine-liart.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
-}))
-
+};
 
 app.use(cors(corsOptions));
-
-
 app.options('*', cors(corsOptions));
+
+// app.use(cors({
+//     origin: "https://task-manager-frontend-nine-liart.vercel.app",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+// }))
+
+
+// app.use(cors(corsOptions));
+
+
+// app.options('*', cors(corsOptions));
 
 
 app.use(express.json())
